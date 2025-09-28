b5564e075765d5accb589b17bd0a37d6be6449b4
Marcel S <marcel.simma@zewas.at>
2025-09-28 13:46:51 +0200

    initial commit

---
+++ src/MarcelSimma/Program.cs
@@ -0,0 +1,17 @@
+﻿// Program.cs
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        /*
+        Die static void Main Funktion bleibt IMMER in der Program.cs. 
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Welt!");
+        }
+    }
+}
