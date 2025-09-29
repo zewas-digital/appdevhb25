ae49974e86b6bc88306793733377773e7fddc594
SashaMat <sasha.mattes@ima-systems.com>
2025-09-29 16:20:21 +0200

    BeispieleEinheit01

---
+++ src/SashaLeonardoMattes/Aufgabe05/PrintLine.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Globalization;
+
+namespace Appdevhb25.SashaLeonardoMattes.Aufgabe05
+{
+
+    public class PrintLine
+    {
+
+        public static void StartPrintLine()
+        {
+            Console.WriteLine("----------Aufgabe 05----------");
+            int amount;
+
+            Console.WriteLine("Wie viel x möchtst du ausgeben?");
+            amount = checkUserInput();
+
+            PrintX(amount);
+        }
+
+        static void PrintX(int amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                Console.Write("x");
+            }
+        }
+
+        static int checkUserInput()
+        {
+            bool checkInput;
+            int result;
+            do
+            {
+                checkInput = int.TryParse(Console.ReadLine(), out result);
+
+                if (checkInput == false)
+                {
+                    Console.WriteLine("Bitte nur Ganzzahlen eingeben");
+                }
+            } while (checkInput == false);
+
+            return result;
+        }
+
+    }
+}
--- src/SashaLeonardoMattes/Program.cs
+++ src/SashaLeonardoMattes/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+
+namespace Appdevhb25.SashaLeonardoMattes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*SashaLeonardoMattes.Aufgabe01.PrintA.StartPrintA();
+            SashaLeonardoMattes.Aufgabe02.MultiplicationTable.StartMultiplicationTable();
+            SashaLeonardoMattes.Aufgabe03.SumInterval.StartSumInterval();
+            SashaLeonardoMattes.Aufgabe04.DividableTest.StartDividableTest();
+            SashaLeonardoMattes.Aufgabe05.PrintLine.StartPrintLine();
+            SashaLeonardoMattes.Aufgabe06.PrintRectangle.StartPrintRectangle();*/
+            SashaLeonardoMattes.Aufgabe07.PrintEmptySqare.StartPrintEmptySqare();
+            //SashaLeonardoMattes.BonusAufgabe01.PrintDeltoid.StartPrintDeltoid();
+        }
+    }
+}
+
