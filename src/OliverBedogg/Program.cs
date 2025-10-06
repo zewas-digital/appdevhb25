3789a8f36383ef78626b263efb44089384af318f
Oliver Bedogg <oliver@ions.at>
2025-10-06 10:07:57 +0200

    primitive datentypen

--- src/OliverBedogg/Program.cs
+++ src/OliverBedogg/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Program.cs
+using System;
+using System.ComponentModel;
+
+namespace Appdevhb25.OliverBedogg
+{
+    internal class Program
+    {
+
+        /*
+        Die static void Main-Funktion bleibt IMMER in der Program.cs. 
+        */
+
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            datatypes.PrimitiveDatatypes.Start();
+        }
+    }
+}
