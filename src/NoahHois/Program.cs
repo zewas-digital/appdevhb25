2dcb4c4ef1a9cf05446426218801bf07853e1b17
HOIS.Noah <NOHO@bachmann.at>
2025-10-06 16:16:24 +0200

    Lösungen für Aufgabe 19, 20 und 21

--- src/NoahHois/Program.cs
+++ src/NoahHois/Program.cs
@@ -6,22 +6,26 @@ namespace Appdevhb25.NoahHois
     {
         public static void Main()
         {
-            // NoahHois.Aufgabe1.PrintA.Aufgabe1();
-            // NoahHois.Aufgabe2.Mahlreihen.Aufgabe2();
-            // NoahHois.Aufgabe3.SummeUeberBereich.Aufgabe3();
-            // NoahHois.Aufgabe4.Teilbarkeit.Aufgabe4();
-            // NoahHois.Aufgabe5.PrintLine.Aufgabe5();
-            // NoahHois.Aufgabe6.PrintRectangle.Aufgabe6();
-            // NoahHois.Aufgabe7.PrintTriangle.Aufgabe7();
-            // NoahHois.Aufgabe8.PrintTriangle2.Aufgabe8();
-            // NoahHois.Aufgabe9.PrintEmptySqare.Aufgabe9();
-            // NoahHois.Aufgabe10.PrintTriangle3.Aufgabe10();
+            // Aufgabe1.PrintA.Aufgabe1();
+            // Aufgabe2.Mahlreihen.Aufgabe2();
+            // Aufgabe3.SummeUeberBereich.Aufgabe3();
+            // Aufgabe4.Teilbarkeit.Aufgabe4();
+            // Aufgabe5.PrintLine.Aufgabe5();
+            // Aufgabe6.PrintRectangle.Aufgabe6();
+            // Aufgabe7.PrintTriangle.Aufgabe7();
+            // Aufgabe8.PrintTriangle2.Aufgabe8();
+            // Aufgabe9.PrintEmptySqare.Aufgabe9();
+            // Aufgabe10.PrintTriangle3.Aufgabe10();
             // Console.WriteLine(Aufgabe11.CheckPrim.Aufgabe11(7));
             // Aufgabe12.Fibonacci.Aufgabe12(5);
             // Aufgabe12.Fibonacci.Aufgabe12(10);
+            // Aufgabe13.PrimitiveDatentypenSumme.Aufgabe13();
+            Aufgabe14.Faktorial.Aufgabe14();
+
             // BonusAufgabe1.PrintDeltoid.BonusAufgabe1();
             // BonusAufgabe2.PrintCricle.BonusAufgabe2(7);
             // BonusAufgabe3.PrintChristmasTree.BonusAufgabe3();
+
         }
     }
 }
