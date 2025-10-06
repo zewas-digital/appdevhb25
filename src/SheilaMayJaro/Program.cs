1f320eae93ea79e55d8a8ce1bba8a3a61427fc06
SheilaMayJaro <SheilaMay.Jaro@illwerkevkw.at>
2025-10-06 13:09:10 +0200

    CommitTest

--- src/SheilaMayJaro/Program.cs
+++ src/SheilaMayJaro/Program.cs
@@ -1,2 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//Program.cs
+using System; 
+
+namespace Appdevhb25.SheilaMayJaro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Aufgabe01.PrintA.Start();
+            // Aufgabe02.TimeTable.Start();
+            // Aufgabe03.SumOverRange.Start();
+            // Aufgabe04.Divisibility.Start();
+            // Aufgabe05.PrintLine.Start();
+            // Aufgabe06.PrintRectangle.Start();
+            // Aufgabe07.PrintTriangle.Start();
+            // Aufgabe08.PrintTriangle2.Start();
+            // Aufgabe09.PrintEmptySquares.Start();
+            // Aufgabe10.PrintTriangle3.Start();
+            // Aufgabe10.PrintTriangle3.PrintTriangle(5);
+            // Console.WriteLine();
+            // Aufgabe11.CheckPrime.Start();
+            // Console.WriteLine(Aufgabe11.CheckPrime.PrimeNumber(25));
+            // Console.WriteLine(Aufgabe11.CheckPrime.PrimeNumber(456));
+            // Console.WriteLine(Aufgabe11.CheckPrime.PrimeNumber(13));
+            // Aufgabe12.Fibonacci.Start();
+            // Aufgabe13.PrintDeltoid.Start();
+            // Aufgabe13.PrintDeltoid.PrintRhombus(13);
+            // Console.WriteLine(); 
+            // Aufgabe13.PrintDeltoid.PrintRhombus(7);
+            // Console.WriteLine(); 
+            //Aufgabe13.PrintDeltoid.PrintRhombus(5); 
+            // Aufgabe14.PrintCircle.Start(); 
+            // Aufgabe14.PrintCircle.PrintCircleMethod(10);
+            Aufgabe15.ChristmasTree.Start();
+            //Aufgabe15.ChristmasTree.PrintChristmasTree(9); 
+            
+            
+        
+            
+
+
+         
+        }
+    }
+}
