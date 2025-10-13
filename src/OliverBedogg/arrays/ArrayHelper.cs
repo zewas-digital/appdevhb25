b47f95c3cbb419093327a390ef855925f893f78c
Oliver Bedogg <oliver@ions.at>
2025-10-13 15:48:18 +0200

    Updated ArrayHelper

--- src/OliverBedogg/arrays/ArrayHelper.cs
+++ src/OliverBedogg/arrays/ArrayHelper.cs
@@ -29,6 +29,42 @@ namespace Appdevhb25.OliverBedogg.arrays
             }
             Console.WriteLine("---");
         }
-        
+
+        public static void PrintArrayForEach(int[] arr)
+        {
+            Console.Write("[");
+            bool first = true;
+            foreach (int value in arr)
+            {
+                if (!first)
+                {
+                    Console.Write(", ");
+                }
+                Console.Write(value);
+                first = false;
+            }
+            Console.WriteLine("]");
+        }
+
+        public static int[] CreateArrayRandom(int size)
+        {
+            int[] arr = new int[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(101);
+            }
+            return arr;
+        }
+
+        public static int[] CreateArrayRandom(int size, int min, int max)
+        {
+            int[] arr = new int[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(min, max);
+            }
+            return arr;
+        }
+
     }
 }
