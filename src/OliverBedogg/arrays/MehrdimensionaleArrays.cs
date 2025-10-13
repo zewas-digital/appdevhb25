b5d8284de99289d15f896d0429752e24c5ea7f07
oliver-ions <oliver@ions.at>
2025-10-13 13:32:24 +0200

    Beispiel für Mehrdimensionale Arrays

--- src/OliverBedogg/arrays/MehrdimensionaleArrays.cs
+++ src/OliverBedogg/arrays/MehrdimensionaleArrays.cs
@@ -17,30 +17,38 @@ namespace Appdevhb25.OliverBedogg.arrays
             zweiDArray[1, 0] = 45;
             // usw.
             ArrayHelper.print2DIntArray(zweiDArray);
+            Console.WriteLine("Länge: " + zweiDArray.Length);
+            Console.WriteLine("Länge 1. Dimension: " + zweiDArray.GetLength(0));
+            Console.WriteLine("Länge 2. Dimension: " + zweiDArray.GetLength(1));
+            Console.WriteLine("---");
 
             // 2D Array, Matrix
             int[,] mehrdimArray = new int[3, 2]
             {
-                { 1, 2 },
-                { 3, 4 },
-                { 5, 6 }
+                { 1, 2 }, // 1. dim: index 0 > 2. dim: index 0, index 1
+                { 3, 4 }, // 1. dim: index 1 > 2. dim: index 0, index 1
+                { 5, 6 } // 1. dim: index 2 > 2. dim: index 0, index 1
             };
-
-            for (int i = 0; i < mehrdimArray.GetLength(0); i++)
+            // wie oben mit gleichen Werten an gleichen Positionen wie zweiDArray befüllt 
+            int[,] mehrdimArray2 = new int[3, 3]
             {
-                for (int j = 0; j < mehrdimArray.GetLength(1); j++)
-                {
-                    Console.Write(mehrdimArray[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("---");
+                { 34, 23, 12},
+                { 45, 0, 0},
+                { 0, 0, 0}
+            };
+            
+            zweiDArray[0, 0] = 1;
+            zweiDArray[0, 1] = 2;
+            ArrayHelper.print2DIntArray(mehrdimArray);
+
 
             // Jagged Array
             int[][] jaggedArray = new int[3][];
             jaggedArray[0] = new int[] { 1, 2 };
             jaggedArray[1] = new int[] { 3, 4, 5 };
             jaggedArray[2] = new int[] { 6 };
+
+            jaggedArray[0][1] = 34;
 
             for (int i = 0; i < jaggedArray.Length; i++)
             {
