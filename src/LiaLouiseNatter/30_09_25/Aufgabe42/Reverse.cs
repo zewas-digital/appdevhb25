26ae395bc7e0e2cf04dc11e5c94351591cb4d3b2
louiselia <lianatter2007@gmail.com>
2025-10-20 08:04:02 +0200

    Alle Aufgaben der letzten Woche

---
+++ src/LiaLouiseNatter/30_09_25/Aufgabe42/Reverse.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Appdevhb25.LiaLouiseNatter.Aufgabe42
+{
+    public class Reverse
+    {
+        public static void Start()
+        {
+            Reverser();
+        }
+        
+        public static void Reverser()
+        {
+            System.Console.Write("Geben Sie ihren Text ein: ");
+            string textUnreversed = Console.ReadLine();
+
+            char[] backwardsArray = textUnreversed.ToCharArray();
+            Array.Reverse(backwardsArray);
+
+            string textReversed = new string(backwardsArray);
+            System.Console.WriteLine($"Ihr umgedrehter Text: {textReversed}");
+
+
+        }
+    }
+}
+
