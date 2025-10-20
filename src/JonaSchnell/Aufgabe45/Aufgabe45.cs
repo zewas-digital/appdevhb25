fd1c4ae69f51722d053bafff88468d4b07bdde87
Jonschn2003 <jona.schnell@gmx.at>
2025-10-20 09:36:31 +0200

    Aufgabe45 Fertigstellung

--- src/JonaSchnell/Aufgabe45/Aufgabe45.cs
+++ src/JonaSchnell/Aufgabe45/Aufgabe45.cs
@@ -9,21 +9,18 @@ namespace Appdevhb25.JonaSchnell.Aufgabe45
         {
             string text = "The quick brown fox jumps over the lazy dog. You can't get rid of me! Assign the address of number to a pointer.";
 
-            char[] textInChar = text.ToCharArray();
-
-            for (int i = 0; i < textInChar.Length; i++)
+            for (int i = 33; i <= 126; i++)
             {
-                //Console.WriteLine(textInChar[i] + " ASCII_Code: " + (int)textInChar[i]);
+                int count = 0;
 
-                for (int j = 0; j < textInChar.Length; j++)
+                foreach (char elem in text)
                 {
-                    int a = 0;
-                    if (j == (int)textInChar[i])
+                    if (elem == (char)i)
                     {
-                        a++;
-                        Console.WriteLine(j);
+                        count++;
                     }
                 }
+                Console.WriteLine((char)i + " = " + count);
             }
         }
     }
