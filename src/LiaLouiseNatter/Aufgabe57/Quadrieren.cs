b7d8e8c387fae8138e1b0f5cfcfc70c8d08c5e84
louiselia <lianatter2007@gmail.com>
2025-10-22 15:34:21 +0200

    Monopoly erweitert

---
+++ src/LiaLouiseNatter/Aufgabe57/Quadrieren.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Security.Principal;
+using System.Text.RegularExpressions;
+
+namespace Appdevhb25.LiaLouiseNatter.Aufgabe57
+{
+    public class Quadrieren
+    {
+        public static void Start()
+        {
+
+        }
+
+        public static void QuadrierenFromFile()
+        {
+            
+        }
+        
+
+
+
+
+        
+
+
+    }
+}
+
