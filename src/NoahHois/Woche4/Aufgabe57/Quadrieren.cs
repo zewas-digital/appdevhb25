0466ab2e63608c53191c050c071723e7e8dd9e90
HOIS.Noah <NOHO@bachmann.at>
2025-10-22 16:42:40 +0200

    Start der Aufgabe 57

---
+++ src/NoahHois/Woche4/Aufgabe57/Quadrieren.cs
@@ -0,0 +1,18 @@
+namespace Appdevhb25.NoahHois.Aufgabe56
+{
+    public class Quadrieren
+    {
+        public static void Start()
+        {
+            int[] numbers = { 4, 6, 13, 45, 5 };
+            using (StreamReader stream = new StreamReader(@"C:\Users\NOHO\Documents\VSCode Projekte\appdevhb25\src\NoahHois\Woche4\Aufgabe56\InputNamen.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter(@"C:\Users\NOHO\Documents\VSCode Projekte\appdevhb25\src\NoahHois\Woche4\Aufgabe56\Output.txt"))
+                {
+
+                }
+            }
+
+        }
+    }
+}
