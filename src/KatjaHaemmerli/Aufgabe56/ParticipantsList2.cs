691b640b88775f139707c51f7dab0b711aec74bd
Katja Haemmerli <katja.haemmerli@huppenkothen.at>
2025-10-28 13:33:49 +0100

    ParticipantsList2 korrigiert

--- src/KatjaHaemmerli/Aufgabe56/ParticipantsList2.cs
+++ src/KatjaHaemmerli/Aufgabe56/ParticipantsList2.cs
@@ -8,31 +8,27 @@ namespace Appdevhb25.KatjaHaemmerli.Aufgabe56
             string datei1 = @"C:/Users/haka/DigitalCampus/appdevhb25/src/KatjaHaemmerli/Aufgabe55/beispieltext.txt";
             string datei2 = @"C:\Users\haka\DigitalCampus\appdevhb25\src\KatjaHaemmerli\Aufgabe56\neuerSpeicherortText.txt";
 
-            // StreamReader den Text lesen
+            // StreamReader: den Text lesen
             using (StreamReader reader = new StreamReader(datei1))
             {
-                // ReadLine() liest die nächste Zeile aus der Datei, gibt die Zeile mit return zurück und speichert sie in die variable line.
-                string line = reader.ReadLine();  // Die Variable line ist ein Zwischenspeicher, weil ich den Wert für die while-Bedingung und für die Ausgabe brauche.
+                using (StreamWriter sw = new StreamWriter(datei2))
 
-                // System.Console.WriteLine(line);
+                {
+                    
+                    // ReadLine() liest die nächste Zeile aus der Datei, gibt die Zeile mit return zurück und speichert sie in die variable line.
+                    string line = reader.ReadLine();  // Die Variable line ist ein Zwischenspeicher, weil ich den Wert für die while-Bedingung und für die Ausgabe brauche.
 
-                int nummerierung = 0;
+                    int nummerierung = 0;
 
-                // while (line != null) // Solange in der variable line ein Text gespeichert ist, soll dieser auf der Konsole ausgegeben und eine neue Zeile eingelesen werden.
-                // {
-                //     line = reader.ReadLine();
-                //     Console.WriteLine($"{++nummerierung,2}. {line}"); // Es werden immer zwei Zeichen für die Aufzählung reserviert und werden von rechts beginnend mit dem Wert der Variable aufgefüllt.
-                // }
+                    while (line != null) // Solange in der variable line ein Text gespeichert ist, soll dieser auf der Konsole ausgegeben und eine neue Zeile eingelesen werden.
+                    {
+                        // ausgelesener Text in eine andere Datei speichern
+                        sw.WriteLine($"{++nummerierung,2}. {line}"); // Schreibt den Inhalt in die neue Datei
 
-                // Text aus einer Textdatei lesen
-                string fileContent = File.ReadAllText(datei1);
+                        line = reader.ReadLine(); // Liest Zeile für Zeile ein nicht der ganze Text auf einmal
+                    }
 
-                // ausgelesener Text in eine andere Datei speichern
-            using (StreamWriter sw = new StreamWriter(datei2))
-            {
-                    sw.WriteLine(fileContent); // Schreibt den Inhalt in die neue Datei
-                System.Console.WriteLine("Der Text wurde in datei2 gespeichert");
-            }
+                }
                 
             }
         }
