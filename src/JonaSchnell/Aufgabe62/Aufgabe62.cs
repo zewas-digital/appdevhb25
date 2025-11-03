6f71040d877c966b64923ed9523144013d6bb5da
Jonschn2003 <jona.schnell@gmx.at>
2025-11-03 11:21:05 +0100

    Finished Aufgabe62

--- src/JonaSchnell/Aufgabe62/Aufgabe62.cs
+++ src/JonaSchnell/Aufgabe62/Aufgabe62.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace Appdevhb25.JonaSchnell.Aufgabe62
 {
     public class Aufgabe62
@@ -6,19 +8,52 @@ namespace Appdevhb25.JonaSchnell.Aufgabe62
         {
             string path = "/home/jona/Dokumente/Coding_Campus/appdevhb25/src/JonaSchnell/Aufgabe62/AufgabeReadAndDisplay.csv";
             StreamReader sr = new StreamReader(path);
+            int y = 0;
+            int digitCounter = 0;
+            int roundCounter = 0;
             while (sr.Peek() >= 0)
             {
                 string document = sr.ReadLine();
                 for (int i = 0; i < document.Length;)
                 {
-                    foreach (char lineChecker in document.Replace(";", "   | "))
+                    roundCounter++;
+                    foreach (char lineChecker in document.Replace(";", " | "))
                     {
+                        digitCounter++;
+
+                        if (lineChecker >= 48 && lineChecker <= 57)
+                        {
+                            Console.Write(" ");
+                        }
+                        if (roundCounter == 1)
+                        {
+                            if (digitCounter == 10)
+                            {
+                                Console.Write(" ");
+                            }
+                        }
+
+
+                        if (lineChecker == 124)
+                        {
+                            y++;
+                        }
+                        if (y == 2)
+                        {
+                            while (digitCounter < 20)
+                            {
+                                Console.Write(" ");
+                                digitCounter++;
+                            }
+                        }
                         Console.Write(lineChecker);
                         i++;
                     }
+                    digitCounter = 0;
+                    y = 0;
                 }
                 System.Console.WriteLine();
-                System.Console.WriteLine();
+                System.Console.WriteLine(new string('-', 30));
             }
 
 
