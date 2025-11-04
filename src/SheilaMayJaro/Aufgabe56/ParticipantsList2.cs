81c884ecb464dd7a4d8176170448d9fbdda22c07
SheilaMayJaro <SheilaMay.Jaro@illwerkevkw.at>
2025-11-04 10:47:43 +0100

    erledigte Aufgaben

--- src/SheilaMayJaro/Aufgabe56/ParticipantsList2.cs
+++ src/SheilaMayJaro/Aufgabe56/ParticipantsList2.cs
@@ -14,35 +14,35 @@ namespace Appdevhb25.SheilaMayJaro.Aufgabe56
             Schreibe in die letzte Zeile die Anzahl der Teilnehmer.
             */
 
-            using (StreamReader FileReader = new StreamReader(@"C:\Users\JASHE\Documents\DigitalCampus2025\appdevhb25\src\SheilaMayJaro\Aufgabe56\input.txt"))
+            using (StreamReader fileReader = new StreamReader(@"C:\Users\JASHE\Documents\DigitalCampus2025\appdevhb25\src\SheilaMayJaro\Aufgabe56\input.txt"))
             {
-                string lineOfFile = FileReader.ReadLine();
+                string lineOfFile = fileReader.ReadLine();
                 int number = 0;
 
                 // System.Console.WriteLine(lineOfFile); //Teilnehmer schonmal ausgeben, damit es bei der nummerierung nicht enthalten ist 
                 // System.Console.WriteLine();
-                // while (lineOfFile != null)
-                // {
-                //     lineOfFile = FileReader.ReadLine();
-                //     System.Console.WriteLine($"{++number,2} {lineOfFile}");
-                // }
+                while (lineOfFile != null)
+                {
+                    lineOfFile = fileReader.ReadLine();
+                    System.Console.WriteLine($"{++number,2} {lineOfFile}");
+                }
                 // System.Console.WriteLine();
                 // System.Console.WriteLine($"{number} Teilnehmer");
 
                 //Wenn die Daten ausgelesen werden, werden sie gelöscht. Damit der Writer Zugriff hat, habe ich es auskommentiert
 
-                using (StreamWriter Filewriter = new StreamWriter(@"C:\Users\JASHE\Documents\DigitalCampus2025\appdevhb25\src\SheilaMayJaro\Aufgabe56\output.txt"))
+                using (StreamWriter filewriter = new StreamWriter(@"C:\Users\JASHE\Documents\DigitalCampus2025\appdevhb25\src\SheilaMayJaro\Aufgabe56\output.txt"))
                 {
-                    Filewriter.WriteLine(lineOfFile);
-                    Filewriter.WriteLine(); 
+                    filewriter.WriteLine(lineOfFile);
+                    filewriter.WriteLine(); 
 
                     while (lineOfFile != null)
                     {
-                        lineOfFile = FileReader.ReadLine(); 
-                        Filewriter.WriteLine($"{++number} {lineOfFile}"); 
+                        lineOfFile = fileReader.ReadLine(); 
+                        filewriter.WriteLine($"{++number} {lineOfFile}"); 
                     }
-                    Filewriter.WriteLine();
-                    Filewriter.WriteLine($"{number} Teilnehmer"); 
+                    filewriter.WriteLine();
+                    filewriter.WriteLine($"{number} Teilnehmer"); 
                 }
             }
         }
