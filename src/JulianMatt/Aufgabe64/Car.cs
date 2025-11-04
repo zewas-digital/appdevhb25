72102f777fd29d8563a98ac8bb9f497ef0026d9a
JulianMatt2009 <jules.matt.09@icloud.com>
2025-11-04 10:52:56 +0100

    TempSave

---
+++ src/JulianMatt/Aufgabe64/Car.cs
@@ -0,0 +1,13 @@
+using System.Globalization;
+
+namespace Appdevhb25.JulianMatt.Aufgabe64
+{
+    public class Car
+    {
+        public string[] Brands;
+
+
+
+
+    }
+}
---
+++ src/JulianMatt/Aufgabe64/Cars.cs
@@ -0,0 +1,20 @@
+using System.Globalization;
+
+namespace Appdevhb25.JulianMatt.Aufgabe64
+{
+    public class Cars
+    {
+
+        public static void Start64()
+        {
+            PartList();
+        }
+
+        public static void PartList()
+        {
+
+        }
+
+
+    }
+}
