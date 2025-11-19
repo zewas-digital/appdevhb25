c8fab7c4c35764a69e3622a6e37ea9b1ebba6db3
selinu04 <selin.ulutuerk@blum.com>
2025-11-19 13:57:07 +0100

    Update Aufgaben

---
+++ src/SelinUlutuerk/ObjektOrientiertesProgrammieren/Bonusaufgaben/Bonusaufgabe87/Abwesenheitsliste.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Appdevhb25.SelinUlutuerk.Bonusaufgabe87
+{
+    class Abwesenheit
+    {
+        public DateTime Start { get; set; }
+        public DateTime Ende { get; set; }
+        
+
+
+        public double DauerInStunden => (Ende - Start).TotalHours;
+
+        public Abwesenheit(DateTime start, DateTime ende)
+        {
+            Start = start;
+            Ende = ende;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Start:g} - {Ende:g}";
+        }
+    }
+    class Teilnehmer
+    {
+        public string Vorname { get; private set; }
+        public string Nachname { get; private set; }
+        public double Stundensatz { get; private set; }
+
+        public List<Abwesenheit> Abwesenheiten { get; } = new List<Abwesenheit>();
+
+        public Teilnehmer(string vorname, string nachname, double stundensatz)
+        {
+            Vorname = vorname;
+            Nachname = nachname;
+            Stundensatz = stundensatz;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Vorname} {Nachname}\t(Stundensatz: {Stundensatz} €/h)";
+        }
+    }
+
+    static class ListenAbrufen
+    {
+        public static void Speichern(List<Teilnehmer> teilnehmerListe, string pfad)
+        {
+            using (var sw = new StreamWriter(pfad))
+            {
+                // Teilnehmerloop
+                foreach (var t in teilnehmerListe)
+                {
+                    sw.WriteLine($"T;{t.Vorname};{t.Nachname};{t.Stundensatz.ToString(CultureInfo.InvariantCulture)}");
+
+                    // Abwesenheitsloop
+                    foreach (var a in t.Abwesenheiten)
+                    {
+                        sw.WriteLine($"A;{a.Start:dd.MM.yyyy HH:mm};{a.Ende:dd.MM.yyyy HH:mm}"
+);
+                    }
+                }
+            }
+        }
+
+        public static List<Teilnehmer> Laden(string pfad)
+        {
+            // erstellen von neuer Liste
+            var liste = new List<Teilnehmer>();
+
+            // falss file leer
+            if (!File.Exists(pfad))
+                return liste;
+
+            // Variable für Teilnehmer
+            Teilnehmer? aktuellerTeilnehmer = null;
+
+
+            foreach (var zeile in File.ReadAllLines(pfad))
+            {
+                // überspringen von Leerzeichen
+                if (string.IsNullOrWhiteSpace(zeile))
+                    continue;
+
+                // aufteilung der Liste
+                var parts = zeile.Split(';');
+
+                if (parts[0] == "T")
+                {
+                    // Daten zuordnen
+                    string vorname = parts[1];
+                    string nachname = parts[2];
+                    double stundensatz = double.Parse(parts[3], CultureInfo.InvariantCulture);
+
+                    // hinzufügen von neuem Teilnehmer
+                    aktuellerTeilnehmer = new Teilnehmer(vorname, nachname, stundensatz);
+                    liste.Add(aktuellerTeilnehmer);
+                }
+                else if ("A" == parts[0] && aktuellerTeilnehmer != null)
+                {
+                    // Datumsformat für einfachere Eingabe
+                    var format = "dd.MM.yyyy HH:mm";
+
+                    // Daten zuordnen
+                    DateTime start = DateTime.ParseExact(parts[1], format, CultureInfo.InvariantCulture);
+                    DateTime ende = DateTime.ParseExact(parts[2], format, CultureInfo.InstalledUICulture);
+                    string fehlgrund = parts[3];
+                    bool entschuldigt = bool.Parse(parts[4]);
+
+                    // hinzufügen zu Abwesenheiten Liste
+                    var abw = new Abwesenheit(start, ende);
+                    aktuellerTeilnehmer.Abwesenheiten.Add(abw);
+                }
+            }
+            return liste;
+        }
+    }
+}
