bc6d8cb5e25f0a1006dcd70a6a08043a5be8622b
Julian Matt <julian.matt@doppelmayr.com>
2025-12-01 08:44:47 +0100

    Done With Execice 99 I did all the things you needed to do for the SQL Exercices and missed nothing

--- src/JulianMatt/Aufgabe96/Figure.cs
+++ src/JulianMatt/Aufgabe96/Figure.cs
@@ -2,16 +2,16 @@ namespace Appdevhb25.JulianMatt.Aufgabe96;
 
 public abstract class Figure
 {
-    public string Name{get; protected set;}
+    public string Name { get; protected set; }
 
-    public string ElementaryType{get; protected set;}
+    public string ElementaryType { get; protected set; }
 
     public abstract double Circumference();
     public abstract double Area();
 
     public override string ToString()
     {
-    return $"Name: {Name}\nTyp: {ElementaryType}";
+        return $"Name: {Name}\nTyp: {ElementaryType}";
     }
 
 }
