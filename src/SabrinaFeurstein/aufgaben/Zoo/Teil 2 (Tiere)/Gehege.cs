6d4a262841ef27ef8b144722ae6abbcdd9510092
sabrina-feurstein <sabrina.feurstein86@gmail.com>
2025-12-09 14:58:17 +0100

    Zoo, Teil 2 (Tiere): Die Enumeration Gattung wurde erstellt und in der Tier-Klasse als Datentyp, anstelle von string verwendet. In der Gehege Klasse und der Start Methode wurden der Datentyp für die Gattung und der Gattungsparameter angepasst. Im UML-Diagramm wurde die Enummeration Gattung ergänzt und der Datentyp für die Gattung in der Tierklasse abgeändert.

--- src/SabrinaFeurstein/aufgaben/Zoo/Teil 2 (Tiere)/Gehege.cs
+++ src/SabrinaFeurstein/aufgaben/Zoo/Teil 2 (Tiere)/Gehege.cs
@@ -5,16 +5,16 @@ namespace Appdevhb25.SabrinaFeurstein.aufgaben.zoo.teil2
         public string Name { get; private set; } = name;
         public List<Tier> Tiere { get; private set; } = new List<Tier>();
 
-        public void TierHinzufuegen(string name, string gattung)
+        public void TierHinzufuegen(string name, Gattung gattung)
         {
             Tiere.Add(new Tier(name, gattung));
         }
-        public void TierEntfernen(string name, string gattung)
+        public void TierEntfernen(string name)
         {
             bool tierEntfernt = false;
             foreach (Tier tier in Tiere)
             {
-                if (tier.Name.Contains(name, StringComparison.OrdinalIgnoreCase) && tier.Gattung.Contains(gattung, StringComparison.OrdinalIgnoreCase))
+                if (tier.Name.Contains(name, StringComparison.OrdinalIgnoreCase))
                 {
                     Tiere.Remove(tier);
                     tierEntfernt = true;
