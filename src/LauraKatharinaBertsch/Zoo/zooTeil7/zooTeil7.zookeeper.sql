8d2b5c08313af75d425304f6f97232243e9bf9d2
LaKaBe1605 <laura.bertsch@liebherr.com>
2025-12-16 16:06:33 +0100

    Zoo Teil7 angefangen in SQL einfügen

---
+++ src/LauraKatharinaBertsch/Zoo/zooTeil7/zooTeil7.zookeeper.sql
@@ -0,0 +1,8 @@
+SELECT * FROM zoo.zookeeper;
+
+INSERT INTO zookeeper (ZookeeperID, Name)
+VALUES (1, 'Peter');
+INSERT INTO zookeeper (ZookeeperID, Name)
+VALUES (2, 'Anneliese');
+INSERT INTO zookeeper (ZookeeperID, Name)
+VALUES (3, 'Hubert');
---
+++ src/LauraKatharinaBertsch/Zoo/zooTeil7/zooTeil7.zookeeper_enclosure.sql
@@ -0,0 +1,8 @@
+SELECT * FROM zoo.zookeeper_enclosure;
+
+INSERT INTO zookeeper_enclosure (ZookeeperID, EnclosureID)
+VALUES (1, 1);
+INSERT INTO zookeeper_enclosure (ZookeeperID, EnclosureID)
+VALUES (2, 2);
+INSERT INTO zookeeper_enclosure (ZookeeperID, EnclosureID)
+VALUES (3, 3);
