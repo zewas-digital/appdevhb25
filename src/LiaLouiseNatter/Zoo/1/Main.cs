e7e1c211dc866f64b07ebf7d4718419b81bc0042
Lia Louise Natter <lia-louise.natter@doppelmayr.com>
2025-12-16 10:35:25 +0100

    Zooaufgabe teil 2. Tierklasse und gehegeklasse vollständig implementiert. Ausgabe ebenfalls eingebaut. Zooaufgabe Teil 3: Futter KLasse und Futterbedarf implementiert. Futter und Menge wird in Dictionary gespeichert. Ausgabe ebenfalls fertig. UML Diagramm updated

--- src/LiaLouiseNatter/Zoo/1/Main.cs
+++ src/LiaLouiseNatter/Zoo/1/Main.cs
@@ -4,28 +4,33 @@ namespace Appdevhb25.LiaLouiseNatter.Zoo
     {
         public static void Start()
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             Zoo zoo = new Zoo("Tiergarten Dornbirn", 2022);
-            List<Compound> compoundList = new List<Compound>
-            {
-                new Compound("Alpenwiese"),
-                new Compound("Ried"),
-                new Compound("Terrarium (warm)"),
-                
-            };
-            
-            Compound compound1 = new Compound("Terrarium (kalt)");
-            compoundList.Add(compound1);
-            
-            
-            
-            foreach(Compound compound in compoundList)
-            {
-                zoo.AddCompound(compound);
-            }
-            zoo.PrintZooStructure();
-            zoo.DeleteCompound(compound1);
-            zoo.PrintZooStructure();
 
+            Compound alpenwiese = new Compound("Alpenwiese");
+            Compound ried = new Compound("Ried");
+            Compound terrariumWarm = new Compound("Terrarium (warm)");
+
+            Food heu = new Food("Heu", "kg", 0.5);
+            FoodRequirements foodRequirements = new FoodRequirements(heu, 20);
+
+            Food wasser = new Food("Wasser", "L", 0.02);
+            FoodRequirements foodRequirements1 = new FoodRequirements(wasser, 100.05);
+
+            alpenwiese.AddAnimal(new Animal("Rijska", "Kuh", foodRequirements));
+
+            ried.AddAnimal(new Animal("Garmond", "Storch", foodRequirements));
+            ried.AddAnimal(new Animal("Hugo", "Storch", foodRequirements1));
+            ried.AddAnimal(new Animal("Idaxis", "Storch", foodRequirements1));
+
+            zoo.AddCompound(alpenwiese);
+            zoo.AddCompound(ried);
+            zoo.AddCompound(terrariumWarm);
+            zoo.UpdateFoodList();
+
+            zoo.PrintZooStructure();
+            zoo.PrintFoodCosts();
         }
+
     }
 }
